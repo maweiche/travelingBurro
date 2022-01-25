0ccda39e77a25f71a2ddb2f12d9cfca6c975cfc7
Matt Weichel <maweiche@gmail.com>
2022-01-25 15:34:02 -0600

    created src files and header for beginning of page

---
+++ burrotrip/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function Header(){
+    retrun (
+        <header className='header'>
+            <h1>The Traveling Burro</h1>
+            <h2>with Becca & Matt!</h2>
+        </header>
+    );
+}
+
+export default Header;
